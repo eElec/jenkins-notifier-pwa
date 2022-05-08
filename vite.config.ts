77fc892bfeb0b55ba2923d761261d6ef2e1441ae
Adrish Aditya <adrishaditya@gmail.com>
2022-05-08 16:22:52 +0530

    fix: mode to enable sourcemap

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
 	plugins: [
 		react({
 			jsxImportSource: '@emotion/react',
@@ -55,6 +55,6 @@ export default defineConfig({
 		}),
 	],
 	build: {
-		sourcemap: process.env.NODE_ENV === 'development',
+		sourcemap: mode === 'development',
 	},
-});
+}));
